ee4f9dbede33d9bd121a6676850eed27d8ff10e5
nathan79-c <19kt210@gmail.com>
2025-01-25 14:44:40 +0200

    creation de la logic metier et implementation de la classe mainActivity

--- app/src/main/java/com/example/screenvideo/ui/MainActivity.kt
+++ app/src/main/java/com/example/screenvideo/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.screenvideo.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,37 +14,47 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.screenvideo.data.ScreenRecorderManager
 import com.example.screenvideo.ui.theme.ScreenVideoTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var screenRecorderManager: ScreenRecorderManager
+    private lateinit var screenCaptureIntentLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        screenRecorderManager = ScreenRecorderManager(this)
+
+        // Register a launcher to get MediaProjection permission
+        screenCaptureIntentLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                screenRecorderManager.startRecording(result.resultCode, result.data!!)
+            }
+        }
+
         setContent {
             ScreenVideoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ScreenRecordApp(
+                    onStartRecording = {
+                        val projectionIntent = screenRecorderManager.getProjectionIntent()
+                        screenCaptureIntentLauncher.launch(projectionIntent)
+                    },
+                    onStopRecording = {
+                        screenRecorderManager.stopRecording()
+                    }
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ScreenVideoTheme {
-        Greeting("Android")
+
     }
 }
