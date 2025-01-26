2c5800fc5e970269df353b4c4a6479dfff5cf9cd
nathan79-c <19kt210@gmail.com>
2025-01-26 09:29:31 +0200

    center the card

--- app/src/main/java/com/example/screenvideo/ui/MainActivity.kt
+++ app/src/main/java/com/example/screenvideo/ui/MainActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.screenvideo.ScreenCaptureService
 import com.example.screenvideo.ui.theme.ScreenVideoTheme
@@ -17,10 +21,15 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ScreenVideoTheme {
-                ScreenRecordApp(
-                    onStartService = { startScreenCaptureService() },
-                    onStopService = { stopScreenCaptureService() }
-                )
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    ScreenRecordApp(
+                        onStartService = { startScreenCaptureService() },
+                        onStopService = { stopScreenCaptureService() }
+                    )
+                }
             }
         }
     }
