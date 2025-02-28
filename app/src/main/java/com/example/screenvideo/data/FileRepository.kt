74edef45d6c21663cf1e269dd3c669fc2b0f42ed
nathan79-c <19kt210@gmail.com>
2025-02-28 09:01:26 +0200

    creation of repository File

---
+++ app/src/main/java/com/example/screenvideo/data/FileRepository.kt
@@ -0,0 +1,12 @@
+package com.example.screenvideo.data
+
+class FileRepository {
+
+    fun createFile():FileSave{
+
+    }
+
+    fun deleteFile(fileSave: FileSave):Boolean{
+
+    }
+}
--- app/src/main/java/com/example/screenvideo/data/FileSave.kt
+++ app/src/main/java/com/example/screenvideo/data/FileSave.kt
@@ -2,5 +2,6 @@ package com.example.screenvideo.data
 
 data class FileSave(
     val name :String,
-    val type :String
+    val type :String,
+    val location :String
 )
