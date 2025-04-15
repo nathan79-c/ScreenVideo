b5435006074b3f317ce9f1bf59a795003b36b3af
nathan79-c <19kt210@esisalama.org>
2025-04-15 08:33:37 +0200

    class service : adjust file

--- app/src/main/java/com/example/screenvideo/ScreenCaptureService.kt
+++ app/src/main/java/com/example/screenvideo/ScreenCaptureService.kt
@@ -1,29 +1,17 @@
 package com.example.screenvideo
 
 
-import android.hardware.display.VirtualDisplay
-import android.util.DisplayMetrics
-import android.view.Surface
-import android.os.Binder
-import android.view.WindowManager
-
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.Service
+import android.content.ComponentName
 import android.content.Intent
-import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
 import android.media.MediaRecorder
 import android.media.projection.MediaProjection
-import android.media.projection.MediaProjectionManager
 import android.os.IBinder
-import android.os.Build
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat.getSystemService
 
-class ScreenCaptureService(mediaProjection: MediaProjection):Service(){
-
+class ScreenCaptureService:Service(){
+    private var mediaProjection: MediaProjection? = null
+    private var virtualDisplay: VirtualDisplay? = null
     val displayMetrics = resources.displayMetrics
     private lateinit var mediaRecorder: MediaRecorder
 
@@ -31,13 +19,11 @@ class ScreenCaptureService(mediaProjection: MediaProjection):Service(){
         return null
 
     }
-   var virtualDisplay = mediaProjection.createVirtualDisplay(
-       "ScreenCapture",
-       displayMetrics.widthPixels,
-       displayMetrics.heightPixels, displayMetrics.densityDpi,
-       DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-       mediaRecorder.surface,
-       null, null)!!
+    override fun startService(intent: Intent?): ComponentName? {
+        super.startService(intent)
+
+        return TODO("Provide the return value")
+    }
 
 }
 
