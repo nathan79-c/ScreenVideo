4e2f30a4e84f0cadedecd11d3fb49bf5499f77e0
nathan79-c <19kt210@esisalama.org>
2025-04-19 09:52:03 +0200

    creation of permission of mediaProjection

--- app/src/main/java/com/example/screenvideo/ScreenCaptureService.kt
+++ app/src/main/java/com/example/screenvideo/ScreenCaptureService.kt
@@ -1,21 +1,27 @@
 package com.example.screenvideo
 
 
+import android.app.Activity.RESULT_OK
 import android.app.Service
 import android.content.ComponentName
 import android.content.Intent
 import android.hardware.display.VirtualDisplay
 import android.media.MediaRecorder
 import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
 import android.os.Handler
 import android.os.IBinder
 import android.view.Surface
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 
 class ScreenCaptureService:Service(){
-    private var mediaProjection: MediaProjection? = null
+    val mediaProjectionManager = getSystemService(MediaProjectionManager::class.java)
+    var mediaProjection: MediaProjection?= null
     private var virtualDisplay: VirtualDisplay? = null
     val displayMetrics = resources.displayMetrics
     private lateinit var mediaRecorder: MediaRecorder
+
+
 
     override fun onBind(p0: Intent?): IBinder? {
         return null
@@ -27,19 +33,8 @@ class ScreenCaptureService:Service(){
         return TODO("Provide the return value")
     }
 
-    fun createVirtualDisplay(
-        name: String,
-        width: Int,
-        height: Int,
-        dpi: Int,
-        flags: Int,
-        surface: Surface?,
-        callback: VirtualDisplay.Callback?,
-        handler: Handler?
-    ): VirtualDisplay?{
 
-        return null
-    }
+
 
 
 }
--- app/src/main/java/com/example/screenvideo/ui/MainActivity.kt
+++ app/src/main/java/com/example/screenvideo/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -46,6 +47,22 @@ class MainActivity : ComponentActivity() {
 
                     startMediaProjection.launch(mediaProjectionManager.createScreenCaptureIntent())
                 }
+                val mediaProjectionManager = getSystemService(MediaProjectionManager::class.java)
+                var mediaProjection : MediaProjection
+
+                val startMediaProjection = registerForActivityResult(
+                    StartActivityForResult()
+                ) { result ->
+                    if (result.resultCode == RESULT_OK) {
+                        mediaProjection = mediaProjectionManager
+                            .getMediaProjection(result.resultCode, result.data!!)
+                    }
+                }
+
+                startMediaProjection.launch(mediaProjectionManager.createScreenCaptureIntent())
+
+
+
             }
         }
     }
